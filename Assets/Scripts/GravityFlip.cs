983d4a60abbeaa025248024ab93d96d6b0e0dd2d
Thaswika <githubthaswika@gmail.com>
2023-09-30 12:52:50 -0400

    third commit

---
+++ Assets/Scripts/GravityFlip.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityFlip : MonoBehaviour
+{
+    public Rigidbody2D avatarRigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+   
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            avatarRigidbody.gravityScale *= -1f;
+            Vector3 newDirection = transform.localScale;
+         newDirection.y = newDirection.y * -1;
+            Debug.Log(newDirection);
+        }
+        
+    }
+}
